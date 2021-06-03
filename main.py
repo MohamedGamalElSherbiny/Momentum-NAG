de076a21a8f7497df61d4a5a2821ba2c6fe3f7f4
MohamedGamalElSherbiny <mr80_thewizard@me.com>
2021-06-03 14:29:07 +0200

    Final Implementation

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ from sklearn.metrics import r2_score
 N = 50
 LEARNING_RATE = 0.0001
 
+def run_all_functions(inner_data):
+    """Function that takes the change in weights, biases, loss functions, predictions ,
+       and weight and bias as tuple and run all functions defined in this class"""
+    get_r2score(inner_data[3])
+    plot_loss_epochs(inner_data[2])
+    plot_thetas_with_loss(inner_data[2], inner_data[0], inner_data[1])
+    plot_all_regression_lines(inner_data[3])
+    plot_best_regression(inner_data[3])
+
 def plot_data():
     """Function That plots the loss function vs number of trials"""
     plt.scatter(input_data, target_labels)
@@ -89,7 +98,6 @@ def get_gradiant_using_momentum(x, y, alpha, epochs=1, gama=0.8):
     all_theta1 = []
     all_loss_functions = []
     all_hypothesis = []
-    count = 1
     old_vt = (0, 0)
     for _ in range(epochs):
         hypothesis = theta0 + theta1 * x
@@ -104,33 +112,58 @@ def get_gradiant_using_momentum(x, y, alpha, epochs=1, gama=0.8):
         all_loss_functions.append(loss_function)
         old_vt = vt
         magnitude_gradient = np.sqrt(sum(np.array(gradient) ** 2))
-        print(magnitude_gradient)
         if np.round(magnitude_gradient, 6) < 0.0001:
             break
-        print(count)
-        count += 1
     return all_theta0, all_theta1, all_loss_functions, all_hypothesis, theta0, theta1
 
-# a = -2
-# b = 1
-# input_data = np.linspace(0, 20, N)
-# target_labels = a * input_data + b
-# plot_data()
-# data = get_gradiant(input_data, target_labels, LEARNING_RATE, 1000)
-# get_r2score(data[3])
-# plot_loss_epochs(data[2])
-# plot_thetas_with_loss(data[2], data[0], data[1])
-# plot_all_regression_lines(data[3])
-# plot_best_regression(data[3])
+def get_gradiant_using_momentum_nag(x, y, alpha, epochs=1, gama=0.8):
+    """Function that implements (the nesterov accelerated gradient descent) that takes the input data, target labels,
+     learning rate and the number of iterations to be performed then applies the mean  square error method and
+       returns the change in weight as float, bias as float,loss functions as a list ,predictions as a list,
+       and the final weight as float and bias as float"""
+    m = len(x)
+    w = np.zeros(2)
+    all_theta0 = []
+    all_theta1 = []
+    all_loss_functions = []
+    all_hypothesis = []
+    old_vt = (0, 0)
+    for _ in range(epochs):
+        hypothesis = w[0] + w[1] * x
+        all_hypothesis.append(hypothesis)
+        loss_function = (1 / 2 * m) * sum((hypothesis - y) ** 2)
+        w_temp = (w[0] - gama * old_vt[0], w[1] - gama * old_vt[1])
+        hypothesis_temp = w_temp[0] + w_temp[1] * x
+        gradient_w_temp = ((1 / m) * sum(hypothesis_temp - y), (1 / m) * sum((hypothesis_temp - y) * x))
+        next_w = (w_temp[0] - alpha * gradient_w_temp[0], w_temp[1] - alpha * gradient_w_temp[1])
+        vt = (gama * old_vt[0] + alpha * gradient_w_temp[0], gama * old_vt[1] + alpha * gradient_w_temp[1])
+        w = next_w
+        all_theta0.append(w[0])
+        all_theta1.append(w[1])
+        all_loss_functions.append(loss_function)
+        old_vt = vt
+        magnitude_gradient = np.sqrt(sum(np.array(gradient_w_temp) ** 2))
+        if np.round(magnitude_gradient, 6) < 0.0001:
+            break
+    return all_theta0, all_theta1, all_loss_functions, all_hypothesis, w[0], w[1]
+
+a = -2
+b = 1
+input_data = np.linspace(0, 20, N)
+target_labels = a * input_data + b
+data = get_gradiant(input_data, target_labels, LEARNING_RATE, 1000)
+run_all_functions(data)
 
 a = -1
 b = 2
 input_data = np.linspace(0, 20, N)
 target_labels = a * input_data + b
-plot_data()
 data = get_gradiant_using_momentum(input_data, target_labels, LEARNING_RATE, 50)
-get_r2score(data[3])
-plot_loss_epochs(data[2])
-plot_thetas_with_loss(data[2], data[0], data[1])
-plot_all_regression_lines(data[3])
-plot_best_regression(data[3])
+run_all_functions(data)
+
+a = -1
+b = 2
+input_data = np.linspace(0, 20, N)
+target_labels = a * input_data + b
+data = get_gradiant_using_momentum_nag(input_data, target_labels, LEARNING_RATE, 100000)
+run_all_functions(data)
