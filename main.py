09f0ae6e9ea9dd34391abd706170cea70c40adf2
MohamedGamalElSherbiny <mr80_thewizard@me.com>
2021-06-03 06:48:01 +0200

    Implemented the mean square error added plotting for each step calculated the accuracy of the system

---
+++ main.py
@@ -0,0 +1,80 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
+
+def plot_loss_epochs(loss_functions, epochs=1):
+    """Function That plots the loss function vs number of trials"""
+    plt.plot(loss_functions, np.arange(epochs))
+    plt.xlabel("Loss Functions")
+    plt.ylabel("Epochs (Number Of full Iterations)")
+    plt.title("Loss Functions vs Epochs")
+    plt.show()
+
+def plot_thetas_with_loss(loss_functions, theta0, theta1):
+    """Function that plots the loss function vs the change in weight and bias"""
+    plt.plot(theta0, loss_functions)
+    plt.plot(theta1, loss_functions, 'r')
+    plt.yscale("Log")
+    plt.xlabel("Weight and Bias (Weight in red)")
+    plt.ylabel("Loss Function")
+    plt.title("Weights and Biases vs Loss Function")
+    plt.show()
+
+def plot_all_regression_lines(hypothesis_list):
+    """Function that plots the input data  vs the change in prediction line"""
+    for i in hypothesis_list:
+        plt.plot(input_data, i)
+    plt.xlabel("Input Data")
+    plt.ylabel("Change in Hypothesis")
+    plt.title("Input Data vs Change in Hypothesis")
+    plt.show()
+
+def plot_best_regression(hypothesis_list):
+    """Function that plots the input data vs the target data and the best prediction line"""
+    plt.scatter(input_data, target_labels)
+    plt.plot(input_data, hypothesis_list[-1], 'r')
+    plt.xlabel("Input Data")
+    plt.ylabel("Best Hypothesis and Target Labels (Hypothesis in red)")
+    plt.title("Input Data vs Best Hypothesis and Target Labels")
+    plt.show()
+
+def get_r2score(hypothesis_list):
+    """Function that prints the accuracy of the system"""
+    print("Accuracy = " + str(r2_score(target_labels, hypothesis_list[-1])))
+
+def get_gradiant(x, y, alpha, epochs=1):
+    """Function that takes the input data, target labels, learning rate and the number of iterations to be
+    performed then applies the mean  square error method and  returns the change in weight as float, bias as float
+    ,loss functions as a list ,predictions as a list, and the final weight as float and bias as float"""
+    m = len(x)
+    theta0 = 0
+    theta1 = 0
+    all_theta0 = []
+    all_theta1 = []
+    all_loss_functions = []
+    all_hypothesis = []
+    for _ in range(epochs):
+        hypothesis = theta0 + theta1 * x
+        all_hypothesis.append(hypothesis)
+        loss_function = (1 / 2 * m) * sum((hypothesis - y) ** 2)
+        all_loss_functions.append(loss_function)
+        delta_theta0 = (1 / m) * sum(hypothesis - y)
+        delta_theta1 = (1 / m) * sum((hypothesis - y) * x)
+        theta0 = theta0 - alpha * delta_theta0
+        all_theta0.append(theta0)
+        theta1 = theta1 - alpha * delta_theta1
+        all_theta1.append(theta1)
+    return all_theta0, all_theta1, all_loss_functions, all_hypothesis, theta0, theta1
+
+n = 50
+a = -2
+b = 1
+input_data = np.linspace(0, 20, n)
+target_labels = a * input_data + b
+learning_rate = 0.001
+data = get_gradiant(input_data, target_labels, learning_rate, 1000)
+get_r2score(data[3])
+plot_loss_epochs(data[2], 1000)
+plot_thetas_with_loss(data[2], data[0], data[1])
+plot_all_regression_lines(data[3])
+plot_best_regression(data[3])
