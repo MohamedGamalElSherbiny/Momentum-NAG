19e4a89cd55afd441bb050e36b63bb16f910d0d6
MohamedGamalElSherbiny <mr80_thewizard@me.com>
2021-06-11 12:02:42 +0200

    Implemented the OOP class on GradientDescent

--- GradientDescent.py
+++ GradientDescent.py
@@ -1,62 +1,76 @@
 import numpy as np
 
-def get_gradiant(x, y, alpha=0.001, epochs=1, theta0=0, theta1=0):
-    """Calculates the gradient descent using mean square error method
-
-    Parameters
-    ----------
-
-    x :         List
-                A list of input data
-
-    y :         List
-                The output data
-
-    alpha:      float, optional
-                The learning rate of the system
-
-    epochs :    int, Optional
-                The number of iterations to be performed on the data
-
-    theta0 :    int, Optional
-                The initial value of the bias
-
-    theta1 :    int, Optional
-                The initial value of the weight
-
-    Returns
-    -------
-
-    Tuple :
-
-    all_theta0 (Biases) : List
-    all_theta1 (Weights): List
-    all_loss_functions (All MSE calculations): List
-    List all_hypothesis (Predictions): List
-    theta0 (Final Bias): int
-    theta1 (Final Weight): int
-    Name of the function: String
-
-    """
-    m = len(x)
-    all_theta0 = []
-    all_theta1 = []
-    all_loss_functions = []
-    all_hypothesis = np.array([])
-    for _ in range(epochs):
-        hypothesis = theta0 + theta1 * x
-        all_hypothesis = np.append(all_hypothesis, hypothesis)
-        loss_function = (1 / (2 * m)) * sum((hypothesis - y) ** 2)
-        all_loss_functions.append(loss_function)
-        gradient = ((1 / m) * sum(hypothesis - y), (1 / m) * sum((hypothesis - y) * x))
-        theta0 = theta0 - alpha * gradient[0]
-        all_theta0.append(theta0)
-        theta1 = theta1 - alpha * gradient[1]
-        all_theta1.append(theta1)
-        if len(all_loss_functions) > 1:
-            if round(all_loss_functions[-1], 5) == round(all_loss_functions[-2], 5):
-                break
-    return all_theta0, all_theta1, all_loss_functions, all_hypothesis, theta0, theta1, "Gradient Descent"
-
 class GradientDescent:
-    pass
+
+    def __init__(self, x, y, alpha=0.001, epochs=1, theta0=0, theta1=1):
+        """
+         Parameters
+        ----------
+
+        x :         List
+                    A list of input data
+
+        y :         List
+                    The output data
+
+        alpha:      float, optional
+                    The learning rate of the system
+
+        epochs :    int, Optional
+                    The number of iterations to be performed on the data
+
+        theta0 :    int, Optional
+                    The initial value of the bias
+
+        theta1 :    int, Optional
+                    The initial value of the weights
+
+        """
+        self.x = x
+        self.y = y
+        self.alpha = alpha
+        self.epochs = epochs
+        self.theta0 = theta0
+        self.theta1 = theta1
+        self.name = "Gradient Descent"
+        self._get_gradiant_tuple = self.get_gradiant()
+
+    def get_gradiant_data(self):
+        return self._get_gradiant_tuple
+
+    def get_gradiant(self):
+        """Calculates the gradient descent using mean square error method
+
+        Returns
+        -------
+
+        Tuple :
+
+        all_theta0 (Biases) : List
+        all_theta1 (Weights): List
+        all_loss_functions (All MSE calculations): List
+        List all_hypothesis (Predictions): List
+        theta0 (Final Bias): int
+        theta1 (Final Weight): int
+        Name of the function: String
+
+        """
+        m = len(self.x)
+        all_theta0 = []
+        all_theta1 = []
+        all_loss_functions = []
+        all_hypothesis = np.array([])
+        for _ in range(self.epochs):
+            hypothesis = self.theta0 + self.theta1 * self.x
+            all_hypothesis = np.append(all_hypothesis, hypothesis)
+            loss_function = (1 / (2 * m)) * sum((hypothesis - self.y) ** 2)
+            all_loss_functions.append(loss_function)
+            gradient = ((1 / m) * sum(hypothesis - self.y), (1 / m) * sum((hypothesis - self.y) * self.x))
+            self.theta0 -= self.alpha * gradient[0]
+            all_theta0.append(self.theta0)
+            self.theta1 -= self.alpha * gradient[1]
+            all_theta1.append(self.theta1)
+            if len(all_loss_functions) > 1:
+                if round(all_loss_functions[-1], 5) == round(all_loss_functions[-2], 5):
+                    break
+        return all_theta0, all_theta1, all_loss_functions, all_hypothesis, self.theta0, self.theta1, self.name
--- main.py
+++ main.py
@@ -1,42 +1,43 @@
-import GradientDescent
-import ImplementAdagrad
-import ImplementAdam
-import ImplementMomentum
-import ImplementMomentumNAG
-import ImplementRMSProp
-import MiniBatchGradientDescent
-import StochasticGradientDescent
+from GradientDescent import GradientDescent
+# import ImplementAdagrad
+# import ImplementAdam
+# import ImplementMomentum
+# import ImplementMomentumNAG
+# import ImplementRMSProp
+# import MiniBatchGradientDescent
+# import StochasticGradientDescent
 from GenerateData import generate_random_data
 from Plot import run_all_functions
 
 input_data, target_labels = generate_random_data(-2, 1)
-data = GradientDescent.get_gradiant(input_data, target_labels, epochs=1000)
+gd = GradientDescent(input_data, target_labels, epochs=1000)
+data = gd.get_gradiant_data()
 run_all_functions(data, input_data, target_labels)
-
-input_data, target_labels = generate_random_data(-1, 2)
-data = ImplementMomentum.get_gradiant_using_momentum(input_data, target_labels, epochs=50)
-run_all_functions(data, input_data, target_labels)
-
-input_data, target_labels = generate_random_data(-1, 2)
-data = ImplementMomentumNAG.get_gradiant_using_momentum_nag(input_data, target_labels, epochs=1000)
-run_all_functions(data, input_data, target_labels)
-
-input_data, target_labels = generate_random_data(-2, 1, n=100)
-data = MiniBatchGradientDescent.using_mini_batch(input_data, target_labels, epochs=1000)
-run_all_functions(data, input_data, target_labels)
-
-input_data, target_labels = generate_random_data(-2, 1)
-data = StochasticGradientDescent.stochastic_GD(input_data, target_labels, 3)
-run_all_functions(data, input_data, target_labels)
-
-input_data, target_labels = generate_random_data(-1, 2)
-data = ImplementAdagrad.implement_adagrad(input_data, target_labels, epochs=1000, alpha=0.1)
-run_all_functions(data, input_data, target_labels)
-
-input_data, target_labels = generate_random_data(-1, 2)
-data = ImplementRMSProp.implement_rms_prop(input_data, target_labels, epochs=1000, alpha=0.1)
-run_all_functions(data, input_data, target_labels)
-
-input_data, target_labels = generate_random_data(-1, 2)
-data = ImplementAdam.implement_adam(input_data, target_labels)
-run_all_functions(data, input_data, target_labels)
+#
+# input_data, target_labels = generate_random_data(-1, 2)
+# data = ImplementMomentum.get_gradiant_using_momentum(input_data, target_labels, epochs=50)
+# run_all_functions(data, input_data, target_labels)
+#
+# input_data, target_labels = generate_random_data(-1, 2)
+# data = ImplementMomentumNAG.get_gradiant_using_momentum_nag(input_data, target_labels, epochs=1000)
+# run_all_functions(data, input_data, target_labels)
+#
+# input_data, target_labels = generate_random_data(-2, 1, n=100)
+# data = MiniBatchGradientDescent.using_mini_batch(input_data, target_labels, epochs=1000)
+# run_all_functions(data, input_data, target_labels)
+#
+# input_data, target_labels = generate_random_data(-2, 1)
+# data = StochasticGradientDescent.stochastic_GD(input_data, target_labels, 3)
+# run_all_functions(data, input_data, target_labels)
+#
+# input_data, target_labels = generate_random_data(-1, 2)
+# data = ImplementAdagrad.implement_adagrad(input_data, target_labels, epochs=1000, alpha=0.1)
+# run_all_functions(data, input_data, target_labels)
+#
+# input_data, target_labels = generate_random_data(-1, 2)
+# data = ImplementRMSProp.implement_rms_prop(input_data, target_labels, epochs=1000, alpha=0.1)
+# run_all_functions(data, input_data, target_labels)
+#
+# input_data, target_labels = generate_random_data(-1, 2)
+# data = ImplementAdam.implement_adam(input_data, target_labels)
+# run_all_functions(data, input_data, target_labels)
